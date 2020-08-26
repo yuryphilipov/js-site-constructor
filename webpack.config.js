83f21ad592249c304e434a44633b9a31ff7d16cc
Yuriy Philipov <yury.philipov@gmail.com>
2020-08-26 09:18:11 +0800

    initial config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
